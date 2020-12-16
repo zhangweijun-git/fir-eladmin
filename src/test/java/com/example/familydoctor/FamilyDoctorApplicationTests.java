36c543392e348d1faf4027608990bc3784a6b079
ZR20080227-张帅 <939345468@qq.com>
2020-12-16 17:25:40 +0800

    我的第一个项目

---
+++ src/test/java/com/example/familydoctor/FamilyDoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.familydoctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyDoctorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
